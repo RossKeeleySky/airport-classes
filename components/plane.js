9008b90d630ad9aa658ad9947e3fc1699e951f2e
Ross Keeley <ross.keeley@sky.uk>
2021-10-25 10:09:20 +0100

    combined crew and passenger arrays in to one

--- components/plane.js
+++ components/plane.js
@@ -2,10 +2,12 @@ class Plane {
     constructor(company, flightNum, crewMemberCapacity, passengerCapacity) {
         this.company = company;
         this.flightNum = flightNum;
-        this.crewMemberCapacity = crewMemberCapacity;
-        this.passengerCapacity = passengerCapacity;
+        // this.crewMemberCapacity = crewMemberCapacity;
+        // this.passengerCapacity = passengerCapacity;
         this.crewMembers = [];
         this.passengers = [];
+        this.crewCapacityRemaining = crewMemberCapacity - this.crewMembers.length;
+        this.passengerCapacityRemaining = passengerCapacityRemaining - this.passengers.length;
     }
 
     // Crew members are boarded in to seperate array to passengers
@@ -14,7 +16,7 @@ class Plane {
         if (crewMember.flightNum !== this.flightNum) { 
             throw new Error (`Crew member flight number ${crewMember.flightNum} does not match plane flight number ${this.flightNum}. Please check boarding pass`);
         // Checks the plane's crew member capacity has not been reached before boarding
-        } else if (this.crewMembers.length + 1 > this.crewMemberCapacity) { 
+        } else if (this.crewCapacityRemaining = 0) { 
              throw new Error (`${this.company} plane with flight number ${this.flightNum} crew member capacity has already been reached`); 
         } else { this.crewMembers.push(crewMember); }
     }
@@ -23,7 +25,7 @@ class Plane {
         if (passenger.flightNum !== this.flightNum) { 
             throw new Error (`Passenger flight number ${passenger.flightNum} does not match plane flight number ${this.flightNum}. Please check boarding pass`);
         // Checks the plane's passenger capacity has not been reached before boarding
-        } else if (this.passengers.length + 1 > this.passengerCapacity) { 
+        } else if (this.passengerCapacityRemaining = 0) { 
              throw new Error (`${this.company} plane with flight number ${this.flightNum} passenger capacity has already been reached`); 
         } else { this.passengers.push(passenger); }
     }
